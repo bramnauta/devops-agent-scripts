2a6c5abaf8f2fce886b77b3ef828b1a9b2701d1d
Bram Nauta <b.nauta@goldspark.solutions>
2025-03-31 14:35:36 +0200

    Run agents as separate users

--- install-agents.sh
+++ install-agents.sh
@@ -16,18 +16,24 @@ echo
 read -p "Please enter the amount of agents to install: " AGENTCOUNT
 read -p "Please enter the number of the first agent to install (usually 1): " AGENTSTARTID
 
-wget -O /tmp/vsts-agent-linux-x64.tar.gz $ARCHIVEURL
+wget -O /tmp/vsts-agent-linux-x64.tar.gz $ARCHIVEURL || exit
 
 cd /usr/bin
 for AGENTID in $(seq -f "%03g" $AGENTSTARTID $(($AGENTCOUNT + $AGENTSTARTID - 1)))
 do
-  sudo mkdir -p agent$AGENTID
-  sudo mkdir -p /var/vsts/agent$AGENTID/_work
-  cd agent$AGENTID
-  sudo tar zxvf /tmp/vsts-agent-linux-x64.tar.gz
-  ./config.sh --url $DEVOPSURL --auth pat --token $TOKEN --unattended --pool $POOL --agent `hostname`-agent$AGENTID --work /var/vsts/agent$AGENTID/_work --acceptTeeEula
-  sudo ./svc.sh install
-  sudo ./svc.sh start
+  username="agent$AGENTID"
+  sudo mkdir -p agent$AGENTID || exit
+  sudo mkdir -p /var/vsts/agent$AGENTID/_work || exit
+  if ! id "$username" &>/dev/null; then
+    sudo adduser --gecos "" --shell /usr/sbin/nologin --disabled-password --disabled-login --home /var/vsts/agent$AGENTID "$username" || exit
+  fi
+  sudo chown -R $username:$username /var/vsts/agent$AGENTID || exit
+  cd agent$AGENTID || exit
+  sudo tar zxvf /tmp/vsts-agent-linux-x64.tar.gz || (rm /tmp/vsts-agent-linux-x64.tar.gz && exit)
+  ./config.sh --url $DEVOPSURL --auth pat --token $TOKEN --unattended --pool $POOL --agent `hostname`-agent$AGENTID --work /var/vsts/agent$AGENTID/_work --acceptTeeEula || exit
+  sudo chown -R $username:$username /usr/bin/agent$AGENTID || exit
+  sudo ./svc.sh install $username || exit
+  sudo ./svc.sh start || exit
   cd ..
 done
 
--- uninstall-agents.sh
+++ uninstall-agents.sh
@@ -6,6 +6,9 @@
 for dir in /usr/bin/agent[0-9][0-9][0-9]; do
   # Check if the directory exists
   if [ -d "$dir" ]; then
+    # Extract the username from the directory name
+    agentname=$(basename "$dir")
+
     # Go into the directory
     cd "$dir" || exit
 
@@ -13,7 +16,7 @@ for dir in /usr/bin/agent[0-9][0-9][0-9]; do
     if [ -f "./svc.sh" ]; then
       sudo ./svc.sh uninstall
     else
-      echo "svc.sh not found in $dir"
+      echo "WARNING: svc.sh not found in $dir"
     fi
 
     # Go out of the directory
@@ -21,5 +24,14 @@ for dir in /usr/bin/agent[0-9][0-9][0-9]; do
 
     # Remove the directory
     sudo rm -rf "$dir"
+
+    # Delete the user non-interactively, ignore if the user doesn't exist
+    if id "$agentname" &>/dev/null; then
+      sudo userdel -r "$agentname"
+    else
+      echo "WARNING: User $agentname does not exist"
+    fi
+
+    sudo rm -rf "/var/vsts/$agentname"
   fi
 done
